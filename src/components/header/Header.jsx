37c214e39487f184741f8fc71a82f35ed6b1a375
Orazrya <96197345+Orazrya@users.noreply.github.com>
2022-10-23 15:14:07 +0300

    upload

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,14 +7,14 @@ import HeaderSocials from "./HeaderSocials";
 const Header = () => {
   return (
     <header>
-      <div className="container header_container">
+      <div className="container header__container">
         <h5>Hello I`m</h5>
         <h1>Or Azaria</h1>
         <h5>Fullstack Developer</h5>
-        <CTA /> <HeaderSocials />
+        <CTA />
+        <HeaderSocials />
         <div className="me">
           <img src={ME} alt="me" />
-
           <a href="#contact" className="scroll__down">
             Scroll Down
           </a>
