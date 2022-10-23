68cb04e369b23530e157c8a8c3bccf0650f69388
Orazrya <96197345+Orazrya@users.noreply.github.com>
2022-10-23 09:09:43 +0300

    upload

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import "./header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <Header>
+      <div className="container header_container"></div>
+      <h5>Hello I`m</h5>
+      <h1>Or Azaria</h1>
+      <h5>Fullstack Developer</h5>
+    </Header>
+  );
 };
 
 export default Header;
