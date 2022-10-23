c873b05f716e757d7fcb591a7ee78cbcbd2df7da
Orazrya <96197345+Orazrya@users.noreply.github.com>
2022-10-24 01:03:25 +0300

    upload

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -2,7 +2,18 @@ import React from "react";
 import "./about.css";
 
 const about = () => {
-  return <section id="about">about</section>;
+  return (
+    <section id="about">
+      <h1>About Me</h1>
+      <div className="container about__container">
+        <div className="about__me"></div>
+        <div className="about__me-img">
+          <img src="" alt="" />
+        </div>
+      </div>
+      <div className="about__content"></div>
+    </section>
+  );
 };
 
 export default about;
