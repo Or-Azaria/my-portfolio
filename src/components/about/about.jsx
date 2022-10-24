fe6287fbed72a2e891d2ac6b060def8c5695854f
Orazrya <96197345+Orazrya@users.noreply.github.com>
2022-10-24 13:46:42 +0300

    upload

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -3,33 +3,18 @@ import "./about.css";
 
 const about = () => {
   return (
-    <section>
+    <section className="container" id="about">
       <h5>Get To Know</h5>
       <h2>About Me</h2>
 
       <div className="about__content">
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio
-          maiores, corporis asperiores nisi temporibus quidem ratione autem
-          repudiandae non voluptatibus at eaque delectus adipisci eveniet sunt
-          optio dolorem, sed aperiam harum, ea sapiente perspiciatis atque
-          repellat cum! Numquam, ullam? Impedit tempora odio neque consequuntur
-          quod voluptates et laudantium reiciendis! Ipsa saepe fuga enim
-          repellendus assumenda consectetur eveniet eos reiciendis, nesciunt
-          asperiores ipsam corrupti accusantium. Nesciunt dignissimos libero
-          velit tempora sed debitis! Fuga error vero quibusdam ipsa rerum autem
-          quidem eaque, magnam provident earum quam! Sapiente a cupiditate
-          possimus, iusto minima repellat, laudantium officia eius illo
-          similique ipsam quibusdam omnis quas. Nemo quos aut temporibus
-          perferendis, eius et voluptatibus neque, qui voluptatem ducimus
-          tempore illum expedita nam autem odit perspiciatis. Deserunt illum
-          dolor eos, voluptas, eveniet eum facilis dolorem, cumque aliquam ut
-          rerum libero. Sequi repellat error quis sapiente dignissimos,
-          inventore eveniet iure esse quasi ducimus mollitia vel perspiciatis
-          ratione cum, quas nesciunt repudiandae temporibus ipsam, quae
-          doloremque dolorem. Quam magnam quaerat illo explicabo eveniet, eaque
+          I am a determined, persistent hardworking programmer who gives high
+          service awareness. Stresses the importance of meeting goals and tasks,
+          loves and encourages teamwork. Provides a professional solution, and
+          solves problems effectively with persuasiveness thoroughness, and
+          effective conduct.
         </p>
-        <br />
         <a href="#contact" className="btn btn-primary">
           Le`ts Talk
         </a>
