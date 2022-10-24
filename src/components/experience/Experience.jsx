5378df6fb1f3872a3a4aa35fe249d7ffd8530dd4
Orazrya <96197345+Orazrya@users.noreply.github.com>
2022-10-24 19:28:11 +0300

    upload

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -2,7 +2,11 @@ import React from "react";
 import "./experience.css";
 
 const Experience = () => {
-  return <section id="experience">Experience</section>;
+  return (
+    <section id="experience">
+      <h5>what skills i have</h5>
+    </section>
+  );
 };
 
 export default Experience;
