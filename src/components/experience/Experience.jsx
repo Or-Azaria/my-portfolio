4e802f72fbb9c56f349529ff93346a2e89a928ef
Orazrya <96197345+Orazrya@users.noreply.github.com>
2022-10-24 23:05:22 +0300

    upload

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,10 +1,51 @@
 import React from "react";
 import "./experience.css";
+import { BsPatchCheckFill } from "react-icons/bs";
 
 const Experience = () => {
   return (
     <section id="experience">
       <h5>what skills i have</h5>
+      <h2>my experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>frontend development</h3>
+          <div className="experience__contact">
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>HTML</h4>
+              <small className="text-light">experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>HTML</h4>
+              <small className="text-light">experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>HTML</h4>
+              <small className="text-light">experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>HTML</h4>
+              <small className="text-light">experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>HTML</h4>
+              <small className="text-light">experienced</small>
+            </article>
+            <article className="experience__details">
+              <BsPatchCheckFill />
+              <h4>HTML</h4>
+              <small className="text-light">experienced</small>
+            </article>
+          </div>
+        </div>
+        <div className="experience__backend"></div>
+      </div>
     </section>
   );
 };
