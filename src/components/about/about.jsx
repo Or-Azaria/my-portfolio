fdbd84059df3179c86f43043270e5635ab5803a1
Orazrya <96197345+Orazrya@users.noreply.github.com>
2022-10-24 11:24:22 +0300

    upload

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -4,14 +4,35 @@ import "./about.css";
 const about = () => {
   return (
     <section id="about">
+      <h5>Get To Know</h5>
       <h1>About Me</h1>
-      <div className="container about__container">
-        <div className="about__me"></div>
-        <div className="about__me-img">
-          <img src="" alt="" />
-        </div>
+      <div className="about__content">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio
+          maiores, corporis asperiores nisi temporibus quidem ratione autem
+          repudiandae non voluptatibus at eaque delectus adipisci eveniet sunt
+          optio dolorem, sed aperiam harum, ea sapiente perspiciatis atque
+          repellat cum! Numquam, ullam? Impedit tempora odio neque consequuntur
+          quod voluptates et laudantium reiciendis! Ipsa saepe fuga enim
+          repellendus assumenda consectetur eveniet eos reiciendis, nesciunt
+          asperiores ipsam corrupti accusantium. Nesciunt dignissimos libero
+          velit tempora sed debitis! Fuga error vero quibusdam ipsa rerum autem
+          quidem eaque, magnam provident earum quam! Sapiente a cupiditate
+          possimus, iusto minima repellat, laudantium officia eius illo
+          similique ipsam quibusdam omnis quas. Nemo quos aut temporibus
+          perferendis, eius et voluptatibus neque, qui voluptatem ducimus
+          tempore illum expedita nam autem odit perspiciatis. Deserunt illum
+          dolor eos, voluptas, eveniet eum facilis dolorem, cumque aliquam ut
+          rerum libero. Sequi repellat error quis sapiente dignissimos,
+          inventore eveniet iure esse quasi ducimus mollitia vel perspiciatis
+          ratione cum, quas nesciunt repudiandae temporibus ipsam, quae
+          doloremque dolorem. Quam magnam quaerat illo explicabo eveniet, eaque
+        </p>
+        <br />
+        <a href="#contact" className="btn btn-primary">
+          Le`ts Talk
+        </a>
       </div>
-      <div className="about__content"></div>
     </section>
   );
 };
