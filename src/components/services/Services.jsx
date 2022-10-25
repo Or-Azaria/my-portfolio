96bd0d8dedc5c5d9bb9500a99058ed0d53f9f0ec
Orazrya <96197345+Orazrya@users.noreply.github.com>
2022-10-26 01:59:09 +0300

    add

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import "./services.css";
 
 const Services = () => {
-  return <section id="services">Services</section>;
+  return <section id="services">
+    <h5></h5>
+  </section>;
 };
 
 export default Services;
