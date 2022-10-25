01baea3654e75fc357b94a31833b9e010fb30c5e
Orazrya <96197345+Orazrya@users.noreply.github.com>
2022-10-25 17:01:33 +0300

    upload

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -3,7 +3,7 @@ import "./about.css";
 
 const about = () => {
   return (
-    <section className="container" id="about">
+    <section  id="about" >
       <h5>Get To Know</h5>
       <h2>About Me</h2>
 
