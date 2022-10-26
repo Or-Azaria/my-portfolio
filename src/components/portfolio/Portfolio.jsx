1c3ce6458c227673394014ba70b0e458b1e835d4
Orazrya <96197345+Orazrya@users.noreply.github.com>
2022-10-26 14:14:52 +0300

    upload

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -11,44 +11,48 @@ const Portfolio = () => {
       <h2>portfolio</h2>
       <div className="container portfolio__container">
         <article className="portfolio__item">
-          <div className="portfolio_item-image">
+          <div className="portfolio__item-image">
             <img src={IMG1} alt="fighters" />
           </div>
           <h3>this is a portfolio title</h3>
-          <a
-            href="https://github.com/Or-Azaria/Top-Fighters"
-            className="btn"
-            target="_blank"
-          >
-            Git hub
-          </a>
-          <a
-            href="https://top-fighters.netlify.app"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live demo
-          </a>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com/Or-Azaria/Top-Fighters"
+              className="btn"
+              target="_blank"
+            >
+              Git hub
+            </a>
+            <a
+              href="https://top-fighters.netlify.app"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live demo
+            </a>
+          </div>
         </article>
         <article className="portfolio__item">
-          <div className="portfolio_item-image">
+          <div className="portfolio__item-image">
             <img src={IMG1} alt="fighters" />
           </div>
           <h3>this is a portfolio title</h3>
-          <a
-            href="https://github.com/Or-Azaria/Top-Fighters"
-            className="btn"
-            target="_blank"
-          >
-            Git hub
-          </a>
-          <a
-            href="https://top-fighters.netlify.app"
-            className="btn btn-primary"
-            target="_blank"
-          >
-            Live demo
-          </a>
+          <div className="portfolio__item-cta">
+            <a
+              href="https://github.com/Or-Azaria/Top-Fighters"
+              className="btn"
+              target="_blank"
+            >
+              Git hub
+            </a>
+            <a
+              href="https://top-fighters.netlify.app"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live demo
+            </a>
+          </div>
         </article>
       </div>
     </section>
