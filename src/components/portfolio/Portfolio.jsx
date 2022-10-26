01397a5e8a1f39b0aa016ed0d5410513ed53c279
Orazrya <96197345+Orazrya@users.noreply.github.com>
2022-10-26 11:25:06 +0300

    upload

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,7 +2,12 @@ import React from "react";
 import "./portfolio.css";
 
 const Portfolio = () => {
-  return <section id="portfolio">Portfolio</section>;
+  return (
+    <section id="portfolio">
+      <h5></h5>
+      <h2></h2>
+    </section>
+  );
 };
 
 export default Portfolio;
