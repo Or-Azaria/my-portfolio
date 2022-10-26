8282071e596ecf7f7cab9a57c15f343bebf9425c
Orazrya <96197345+Orazrya@users.noreply.github.com>
2022-10-26 11:04:18 +0300

    upload

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,10 +1,47 @@
 import React from "react";
 import "./services.css";
-
+import { BiCheck } from "react-icons/bi";
 const Services = () => {
-  return <section id="services">
-    <h5></h5>
-  </section>;
+  return (
+    <section id="services">
+      <h5>what i offer</h5>
+      <h2>services</h2>
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>web development</h3>
+          </div>
+
+          <ul className="services__list">
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse
+              </p>
+            </li>
+            <li>
+              <BiCheck className="service__list-icon" />
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse
+              </p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Services;
