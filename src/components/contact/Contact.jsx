6ce467ef388514763db926263efc01b2566d182e
Orazrya <96197345+Orazrya@users.noreply.github.com>
2022-10-26 17:29:54 +0300

    add icond

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,13 +1,25 @@
 import React from "react";
 import "./contact.css";
 import { MdOutlineEmail } from "react-icons/md";
-
+import { RiMessengerLine } from "react-icons/ri";
 const Contact = () => {
   return (
     <section id="contact">
       <h5>get in touch</h5>
       <h2>contact me</h2>
       <div className="container contact__container">
+        <article className="contact__options">
+          <MdOutlineEmail />
+          <h4>Email</h4>
+          <h5>azriaor@gmail.com</h5>
+          <a href="mailto:azriaor@gmail.com"></a>
+        </article>
+        <article className="contact__options">
+          <RiMessengerLine />
+          <h4>messenger</h4>
+          <h5>or azaria</h5>
+          <a href="http://m.me/or.azarya.5"></a>
+        </article>
         <article className="contact__options">
           <MdOutlineEmail />
           <h4>Email</h4>
