971302b5ddfe1ff8b71344e591f695925a4dd85e
Orazrya <96197345+Orazrya@users.noreply.github.com>
2022-11-02 14:13:34 +0200

    upload

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -8,11 +8,14 @@ const about = () => {
 
       <div className="about__content container">
         <p>
-          I am a determined, persistent hardworking programmer who gives high
-          service awareness. Stresses the importance of meeting goals and tasks,
-          loves and encourages teamwork. Provides a professional solution, and
-          solves problems effectively with persuasiveness thoroughness, and
-          effective conduct.
+          I am from Lod, Israel born and raised. since childhood, I always tried
+          to design and create things in my own unique way. Especially when it
+          becomes useful to others.
+          <br />
+          As a former Basketball-player, I know all about teamwork and the
+          importance of being a part of a group, At my coding Bootcamp, it was
+          easy for me to connect and communicate with my classmates which leads
+          to more understanding, and problem-solving much faster!
         </p>
       </div>
     </section>
