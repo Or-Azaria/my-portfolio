3730646c4283d30df9f6d92df748abb5be23f4c7
Orazrya <96197345+Orazrya@users.noreply.github.com>
2022-11-02 21:06:42 +0200

    add

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -68,7 +68,7 @@ const Experience = () => {
             <article className="experience__details">
               <BsPatchCheckFill className="experience__details-icon" />
               <div>
-                <h4>mySQL</h4>
+                <h4>MySQL</h4>
               </div>
             </article>
             <article className="experience__details">
