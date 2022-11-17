741206dcfdff789e68f729b767af476b4913c709
Orazrya <96197345+Orazrya@users.noreply.github.com>
2022-11-17 15:03:43 +0200

    upload

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,30 +1,36 @@
-import React from "react";
-import "./portfolio.css";
-import IMG1 from "../../assets/Screen Shot 2022-10-26 at 11.22.34.png";
-import IMG2 from "../../assets/Screen Shot 2022-10-26 at 17.02.32.png";
-import IMG3 from "../../assets/ticTacToe.png";
+import React from 'react';
+import './portfolio.css';
+import IMG1 from '../../assets/Screen Shot 2022-10-26 at 11.22.34.png';
+import IMG2 from '../../assets/Screen Shot 2022-10-26 at 17.02.32.png';
+import IMG3 from '../../assets/ticTacToe.png';
 
 const data = [
   {
     id: 1,
     image: IMG1,
-    title: "Top-fighters",
-    github: "https://github.com/Or-Azaria/Top-Fighters",
-    demo: "https://top-fighters.netlify.app",
+    title: 'Top-fighters',
+    github: 'https://github.com/Or-Azaria/Top-Fighters',
+    demo: 'https://top-fighters.netlify.app',
   },
   {
     id: 2,
     image: IMG2,
-    title: "Roll-dice",
-    github: "https://github.com/Or-Azaria/roll-dice-game",
-    demo: "https://project-of-dice.netlify.app",
+    title: 'Roll-dice',
+    github: 'https://github.com/Or-Azaria/roll-dice-game',
+    demo: 'https://project-of-dice.netlify.app',
   },
   {
     id: 3,
     image: IMG3,
-    title: "Tic-Tac-Toe",
-    github: "https://github.com/Or-Azaria/Tic-tac-toe",
-    demo: "https://tic-tac-toe-enjoy.netlify.app/",
+    title: 'Tic-Tac-Toe',
+    github: 'https://github.com/Or-Azaria/Tic-tac-toe',
+    demo: 'https://tic-tac-toe-enjoy.netlify.app/',
+  },
+  {
+    id: 4,
+    image:
+      'https://media.istockphoto.com/vectors/coming-soon-no-photo-no-video-available-image-loading-screen-vector-vector-id1396577496?k=20&m=1396577496&s=612x612&w=0&h=DReRiFwJntCZbc2FJf53o3pV5U9XF2tbVraPAaO2M5k=',
+    title: 'Coming Soon',
   },
 ];
 const Portfolio = () => {
