9f20030c391c55015868a225789ffb19f9c280b3
Orazrya <96197345+Orazrya@users.noreply.github.com>
2023-08-04 22:18:24 +0300

    pdf cv

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import React from "react";
-import Header from "./components/header/Header";
-import Nav from "./components/nav/Nav";
-import About from "./components/about/About";
-import Experience from "./components/experience/Experience";
-import Portfolio from "./components/portfolio/Portfolio";
-import Contact from "./components/contact/Contact";
-import Footer from "./components/footer/Footer";
+import React from 'react';
+import Header from './components/header/Header';
+import Nav from './components/nav/Nav';
+import About from './components/about/About';
+import Experience from './components/experience/Experience';
+import Portfolio from './components/portfolio/Portfolio';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 const App = () => {
   return (
     <>
